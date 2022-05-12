4e89f291a657c42ad16e82098b5b09ec0b0970e0
Alessandro Mazzeo <32978759+jojo2234@users.noreply.github.com>
2022-05-12 20:08:51 +0200

    Bug background fixed
    Thanks to tod97

--- renderer.js
+++ renderer.js
@@ -85,7 +85,7 @@ vray.forEach(ele => {
 	ele.addEventListener("click",function(){this.style="border:4px solid #00b761;border-radius:4px;";document.getElementById("cv").style.backgroundImage="url("+this.src+")";let lray=document.getElementById("insfd").childNodes;lray.forEach(elm => {if(elm!=this){elm.style="none";}});});
 });
 let bray=document.getElementById("clbk").childNodes;
-bray.forEach(cbk => {cbk.addEventListener("click",function(){let acbc=this.style.backgroundColor;document.getElementById("cv").style.backgroundColor=acbc;this.style='border:1px solid #00b761;'+'background-color:'+acbc+';';document.getElementById("cv").style.backgroundImage="none";let cray=document.getElementById("clbk").childNodes;cray.forEach(lm => {if(lm!=this){lm.style.borderColor="gray";}})});});
+bray.forEach(cbk => {cbk.addEventListener("click",function(){let acbc=this.style.backgroundImage;document.getElementById("cv").style.backgroundImage=acbc;this.style='border:1px solid #00b761;'+'background-image:'+acbc+';';let cray=document.getElementById("clbk").childNodes;cray.forEach(lm => {if(lm!=this){lm.style.borderColor="gray";}})});});
 
 document.getElementById("csi").addEventListener("click",function(e){if(csiOp==true){csiOp=false;document.getElementById("sfd").style="display:none;";}else{csiOp=true;document.getElementById("sfd").style="display:block;";e.
 stopPropagation();}});
